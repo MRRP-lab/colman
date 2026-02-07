071c25881efd8795d5ea639a4a051fbccf251843
piercecoyle <dev@piercecoyle.com>
2026-02-07 11:17:04 +0000

    add oak d pro description

--- src/oakd_description/setup.py
+++ src/oakd_description/setup.py
@@ -1,4 +1,6 @@
+from glob import glob
 from setuptools import find_packages, setup
+import os
 
 package_name = 'oakd_description'
 
@@ -10,6 +12,10 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/urdf',
+            glob(os.path.join(package_name, 'urdf', '*'))),
+        ('share/' + package_name + '/meshes',
+            glob(os.path.join(package_name, 'meshes', '*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
