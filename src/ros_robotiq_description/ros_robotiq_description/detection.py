69862ba2496322ec757dda92d9a002ca1aacfc0b
Pierce Coyle <dev@piercecoyle.com>
2026-02-24 13:45:53 -0800

    add logs to detection node

--- src/ros_robotiq_description/ros_robotiq_description/detection.py
+++ src/ros_robotiq_description/ros_robotiq_description/detection.py
@@ -10,34 +10,46 @@ class DetectionNode(Node):
     def __init__(self):
         super().__init__("yolo_node")
 
+        # convert between ros images and opencv images
         self.bridge = CvBridge()
+
+        # publishes annotated images
         self.pub_annotated = self.create_publisher(
             Image,
             "/yolo/annotated",
             10
         )
+
         self.latest_image = None
         self.latest_header = None
 
+        # each time a camera info message arrives, run image callback
         self.create_subscription(Image, "/oakd_pro/rgb/image", self.image_callback, 10)
         self.model = YOLO("yolo11m-seg.pt")
 
         # 20hz
         self.create_timer(0.05, self.detect_timer_callback)
-        self.get_logger().info("Detection Node Started.")
+        self.get_logger().debug("Detection Node Started.")
 
     def image_callback(self, msg: Image):
+        """
+        Saves the image to be analyzed
+        """
         self.latest_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         self.latest_header = msg.header
 
     def detect_timer_callback(self):
         if self.latest_image is None:
+            self.get_logger().warn(f"No image found.")
             return
 
         frame = self.latest_image.copy()
 
         # Run model detection
         results = self.model(frame, verbose=False)
+        detections = results[0].boxes
+        num_objects = len(detections)
+        self.get_logger().debug(f"Found {num_objects} objects.")
 
         # Draw detections for visualization
         annotated = results[0].plot()
