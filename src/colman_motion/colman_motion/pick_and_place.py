d2d58b4e97de8425fe57d613532c4b44b77b3b70
piercecoyle <dev@piercecoyle.com>
2026-06-26 03:48:48 +0000

    remove scene manager from pick and place

--- src/colman_motion/colman_motion/pick_and_place.py
+++ src/colman_motion/colman_motion/pick_and_place.py
@@ -1,16 +1,15 @@
 import threading
 
 import rclpy
-from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import PoseStamped
 from moveit.core.kinematic_constraints import construct_joint_constraint
 from moveit.core.robot_state import RobotState
 from moveit.planning import MoveItPy, PlanRequestParameters
-from moveit_msgs.msg import CollisionObject
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
-from shape_msgs.msg import SolidPrimitive
 from std_srvs.srv import Trigger
 
+from colman_motion.scene_manager import SceneManager
 from colman_motion.tag_lookup import TagLookup
 from colman_motion.vacuum_control import VacuumControl
 
@@ -74,50 +73,6 @@ class StopControl(Node):
         response.success = True
         response.message = "Successfully Stopped"
         return response
-
-
-class SceneManager(Node):
-    def __init__(self, moveit):
-        super().__init__("scene_manager")
-        self.planning_scene_monitor = moveit.get_planning_scene_monitor()
-
-    def add_box(self, obj_id, frame_id, dims, pos, orientation_w=1.0):
-        with self.planning_scene_monitor.read_write() as scene:
-            obj = CollisionObject()
-            obj.header.frame_id = frame_id
-            obj.id = obj_id
-
-            box = SolidPrimitive()
-            box.type = SolidPrimitive.BOX
-            box.dimensions = dims
-
-            pose = Pose()
-            pose.position.x, pose.position.y, pose.position.z = pos
-            pose.orientation.w = orientation_w
-
-            obj.primitives.append(box)
-            obj.primitive_poses.append(pose)
-            obj.operation = CollisionObject.ADD
-
-            scene.apply_collision_object(obj)
-            scene.current_state.update()
-
-        self.get_logger().info(f"Added: {obj_id}")
-
-    def remove_object(self, obj_id):
-        with self.planning_scene_monitor.read_write() as scene:
-            obj = CollisionObject()
-            obj.id = obj_id
-            obj.operation = CollisionObject.REMOVE
-            scene.apply_collision_object(obj)
-            scene.current_state.update()
-        self.get_logger().info(f"Removed: {obj_id}")
-
-    def clear_scene(self):
-        with self.planning_scene_monitor.read_write() as scene:
-            scene.remove_all_collision_objects()
-            scene.current_state.update()
-        self.get_logger().info("Cleared all objects")
 
 
 def plan_and_execute(
