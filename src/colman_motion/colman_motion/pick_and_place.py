8b4c75b2317424863c8b0c04a9bdda3c7f14def3
coylep2 <coylep2@kb109-nil-08.cs.wwu.edu>
2026-07-22 14:15:12 -0700

    change pick and place to pattern based stacking

--- src/colman_motion/colman_motion/pick_and_place.py
+++ src/colman_motion/colman_motion/pick_and_place.py
@@ -1,5 +1,6 @@
 import threading
 
+import time
 import rclpy
 from moveit.planning import MoveItPy, PlanRequestParameters
 from rclpy.executors import MultiThreadedExecutor
@@ -9,7 +10,9 @@ from colman_motion.scene_manager import SceneManager
 from colman_motion.tag_lookup import TagLookup
 from colman_motion.vacuum_control import VacuumControl
 
-APPROACH_OFFSET = 0.05
+APPROACH_OFFSET = 0.1
+
+EE_OFFSET = 0.01
 
 EE_DOWN = (1.0, 0.0, 0.0, 0.0)
 
@@ -45,12 +48,12 @@ def main():
         )
 
         fast = PlanRequestParameters(ur, "ompl")
-        fast.max_velocity_scaling_factor = 0.3
-        fast.max_acceleration_scaling_factor = 0.3
+        fast.max_velocity_scaling_factor = 0.7
+        fast.max_acceleration_scaling_factor = 0.7
 
         slow = PlanRequestParameters(ur, "ompl")
-        slow.max_velocity_scaling_factor = 0.1
-        slow.max_acceleration_scaling_factor = 0.1
+        slow.max_velocity_scaling_factor = 0.3
+        slow.max_acceleration_scaling_factor = 0.3
 
         # tool0 up Translation: [0.086, 0.129, 0.701]
 
@@ -58,57 +61,85 @@ def main():
 
         vacuum.release()
 
+        tag_pattern = ["tag_1", "tag_3", "tag_5", "tag_6"]
+
+
         while not stop_event.is_set():
-            if not arm.go_to_configured_pose("Look4", slow):
-                logger.warn("Stopping")
-                return
+            old = None
+            for tag_name in tag_pattern:
+                if old is None:
+                    old = tag_name
+                    logger.warn("continue 1\n\n\n")
+                    continue
+                tag_frame = tag_name
+                if not arm.go_to_configured_pose("Look3", fast):
+                    logger.warn("Stopping")
+                    return
+                tag_pose = None
+                while tag_pose is None:
+                    tag_pose = tag.get_tag_pose(tag_frame)
 
-            tag_pose = tag.get_tag_pose()
+                tag_translation = tag_pose.translation
+                pick_translation = (tag_translation.x, tag_translation.y, tag_translation.z - EE_OFFSET)
+                approach_translation = (
+                    tag_translation.x,
+                    tag_translation.y,
+                    tag_translation.z + APPROACH_OFFSET,
+                )
 
-            if tag_pose is None:
-                continue
+                if not arm.go_to_pose(*approach_translation, *EE_DOWN, fast):
+                    logger.warn("Stopping")
+                    return
 
-            tag_translation = tag_pose.translation
-            pick_translation = (tag_translation.x, tag_translation.y, tag_translation.z)
-            approach_translation = (
-                tag_translation.x,
-                tag_translation.y,
-                tag_translation.z + APPROACH_OFFSET,
-            )
+                if not arm.go_to_pose(*pick_translation, *EE_DOWN, slow):
+                    logger.warn("Stopping")
+                    return
 
-            if not arm.go_to_pose(*approach_translation, *EE_DOWN, slow):
-                logger.warn("Stopping")
-                return
+                vacuum.grasp()
 
-            if not arm.go_to_pose(*pick_translation, *EE_DOWN, slow):
-                logger.warn("Stopping")
-                return
+                if not arm.go_to_pose(*approach_translation, *EE_DOWN, fast):
+                    logger.warn("Stopping")
+                    return
 
-            vacuum.grasp()
+                tag_frame = old
 
-            if not arm.go_to_pose(*approach_translation, *EE_DOWN, slow):
-                logger.warn("Stopping")
-                return
+                if not arm.go_to_configured_pose("Look3", fast):
+                    logger.warn("Stopping")
+                    return
 
-            drop_off_approach = (
-                DROP_OFF[0],
-                DROP_OFF[1],
-                DROP_OFF[2] + APPROACH_OFFSET,
-            )
+                drop_pose = None
+                while drop_pose is None:
+                    drop_pose = tag.get_tag_pose(tag_frame)
 
-            if not arm.go_to_pose(*drop_off_approach, *EE_DOWN, slow):
-                logger.warn("Stopping")
-                return
+                drop_off = (
+                    drop_pose.translation.x,
+                    drop_pose.translation.y,
+                    drop_pose.translation.z + 0.05,
+                )
 
-            if not arm.go_to_pose(*DROP_OFF, *EE_DOWN, slow):
-                logger.warn("Stopping")
-                return
+                drop_off_approach = (
+                    drop_off[0],
+                    drop_off[1],
+                    drop_off[2] + APPROACH_OFFSET,
+                )
 
-            vacuum.release()
-
-            if not arm.go_to_pose(*drop_off_approach, *EE_DOWN, slow):
-                logger.warn("Stopping")
-                return
+                if not arm.go_to_pose(*drop_off_approach, *EE_DOWN, fast):
+                    logger.warn("Stopping")
+                    return
+                
+                if not arm.go_to_pose(*drop_off, *EE_DOWN, slow):
+                    logger.warn("Stopping")
+                    return
+                
+                vacuum.release()
+                
+                if not arm.go_to_pose(*drop_off_approach, *EE_DOWN, slow):
+                    logger.warn("Stopping")
+                    return
+                old = tag_name
+            logger.warn("\nreturning\n")    
+            return
+                
 
     finally:
         vacuum.release()
